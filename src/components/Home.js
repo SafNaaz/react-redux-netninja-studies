c97c2466647a242ac0312a5f07fa5424f2553b23
SAFNAS <iamsafnas@gmail.com>
2020-06-07 23:29:21 +0530

    axios fetch

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-export default function Home() {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-        temporibus dicta quam, ab quibusdam voluptas beatae dolor veniam maiores
-        necessitatibus placeat ratione autem laboriosam, iste aliquam culpa
-        voluptatem. Iusto, velit.
-      </p>
-    </div>
-  );
+export default class Home extends Component {
+  state = {
+    posts: [],
+  };
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      this.setState({
+        posts: res.data.slice(0, 10),
+      });
+    });
+  }
+
+  render() {
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map((post) => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <div className="center">No Posts Yet</div>
+    );
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        {postList}
+      </div>
+    );
+  }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Link, NavLink, withRouter } from "react-router-dom";
 
 function Navbar(props) {
-  setTimeout(() => {
-    props.history.push("/about");
-  }, 4000);
+  //   setTimeout(() => {
+  //     props.history.push("/about");
+  //   }, 4000);
   return (
     <div>
       <nav className="nav-wrapper red darken-3">
