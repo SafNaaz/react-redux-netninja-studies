18f7f3dce917060df31b8ffa129027fb264ae33e
SAFNAS <iamsafnas@gmail.com>
2020-06-09 14:47:28 +0530

    delete post using dispatch and redirect to home

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,11 +2,20 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class Post extends Component {
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id);
+    this.props.history.push("/");
+  };
   render() {
     const post = this.props.post ? (
       <div className="post">
         <h4 className="center">{this.props.post.title}</h4>
         <p>{this.props.post.body}</p>
+        <div className="center">
+          <button className="btn grey" onClick={this.handleClick}>
+            Delete Post
+          </button>
+        </div>
       </div>
     ) : (
       <div className="center">Loading Post...</div>
@@ -27,4 +36,12 @@ const mapStateProps = (state, ownProps) => {
   };
 };
 
-export default connect(mapStateProps)(Post);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deletePost: (id) => {
+      dispatch({ type: "DELETE_POST", id: id });
+    },
+  };
+};
+
+export default connect(mapStateProps, mapDispatchToProps)(Post);
