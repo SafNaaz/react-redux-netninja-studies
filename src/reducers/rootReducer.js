c1e4784ba6ec455f3e058f7d2b65ad39b3c15430
SAFNAS <iamsafnas@gmail.com>
2020-06-09 14:23:37 +0530

    list out sample posts using connect

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,21 @@
 const initState = {
-  posts: [],
+  posts: [
+    {
+      id: "1",
+      title: "lorem lasfjalsfjalsdf",
+      body: "fdsaffffffffffffffff askdfhkajsdfhkasd khasdkjfh",
+    },
+    {
+      id: "2",
+      title: "lorem lasfjalsfjalsdf -2",
+      body: "fdsaffffffffffffffff askdfhkajsdfhkasd khasdkjfh -2",
+    },
+    {
+      id: "3",
+      title: "lorem lasfjalsfjalsdf -3",
+      body: "fdsaffffffffffffffff askdfhkajsdfhkasd khasdkjfh -3",
+    },
+  ],
 };
 
 const rootReducer = (state = initState, action) => {
